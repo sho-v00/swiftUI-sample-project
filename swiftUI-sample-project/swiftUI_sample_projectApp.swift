d7045d44524f8a6aaf57a197d3bf8c8e3d54757d
Sho <developer@shota-ito.com>
2021-08-06 18:41:46 +0900

    Initial Commit

---
+++ swiftUI-sample-project/swiftUI_sample_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_sample_projectApp.swift
+//  swiftUI-sample-project
+//
+//  Created by Shota Ito on 2021/08/06.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_sample_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
